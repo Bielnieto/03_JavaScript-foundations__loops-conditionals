0ce32689afe36743c79046fd5fdbcb0e916d79d4
Biel <biel.nieto@mataro.epiaedu.cat>
2024-11-26 19:26:59 +0100

    update

--- loops-conditionals/loops/exercise_01.js
+++ loops-conditionals/loops/exercise_01.js
@@ -7,11 +7,3 @@
         * If it is less than 5, print "Logged Out with (number)!"
 */
 
-for (let i = 0; i <= 10; i++) {
-    let randomNumber = Math.floor(Math.random() * 10) + 1;
-    if (randomNumber >= 5) {
-        console.log(`Logged In with ${randomNumber}!`);
-    } else {
-        console.log(`Logged Out with ${randomNumber}!`);
-    }
-}
