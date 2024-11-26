9f0d94a7c77381cab001ae7e877045702001b6ed
Biel <biel.nieto@mataro.epiaedu.cat>
2024-11-26 19:29:37 +0100

    while exercices solved

--- loops-conditionals/while/exercise_01.js
+++ loops-conditionals/while/exercise_01.js
@@ -3,3 +3,19 @@
 
     2. Create a while loop that will print out all the odd numbers between 10 and 40.
 */
+
+let evenNumber = 10;
+while (evenNumber <= 40) {
+    if (evenNumber % 2 === 0) {
+        console.log(evenNumber);
+    }
+    evenNumber++;
+}
+
+let oddNumber = 10;
+while (oddNumber <= 40) {
+    if (oddNumber % 2 !== 0) {
+        console.log(oddNumber);
+    }
+    oddNumber++;
+}
